4de9198d80899b225fd01bff5a8af8cd202ca2f7
aripan <aripan017@gmail.com>
2021-02-07 14:15:52 +0100

    Organized the code

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-// APIs
-
-// Search meal by name
-// https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
-
-// List all meals by first letter
-// https://www.themealdb.com/api/json/v1/1/search.php?f=a
-
-// fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=mass")
-//   .then((res) => res.json())
-//   .then((data) => console.log(data.meals));
-
-// fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=b")
-//   .then((res) => res.json())
-//   .then((data) => displayMeals(data.meals));
-//!  .then((data) => console.log(data.meals[0].strMeal));
-
 const searchOption = document.getElementById("search");
 const invalidSearch = document.getElementById("invalid-search");
 const individualMealIfo = document.getElementById("individual-meal-info");
@@ -24,19 +7,23 @@ const showSearchResult = () => {
   individualMealIfo.innerHTML = "";
   allMealsDiv.innerHTML = "";
 
-  const inputValue = document.getElementById("search").value;
-  console.log(inputValue.length);
-  if (inputValue == 0) {
-    invalidSearch.innerHTML = `<h1>INVALID SEARCH...</h1>
-          <h4>Please write the name properly.</h4>
+  if (searchOption.value == 0) {
+    invalidSearch.innerHTML = `
+          <h1>!!!</h1>
+          <h1>INVALID SEARCH...</h1>
+          <h4>Please write the name properly...</h4>
           `;
   } else {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchOption.value}`
+    )
       .then((res) => res.json())
       .then((data) => {
         if (!data.meals) {
-          invalidSearch.innerHTML = `<h1>INVALID SEARCH...</h1>
-          <h4>Please write the name properly.</h4>
+          invalidSearch.innerHTML = `
+            <h1>!!!</h1>
+            <h1>INVALID SEARCH...</h1>
+            <h4>Please write the name properly...</h4>
           `;
         } else {
           invalidSearch.innerHTML = "";
@@ -52,38 +39,38 @@ const displayMeals = (mealName) => {
   mealName.forEach((meal) => {
     const individualMealDiv = document.createElement("div");
     individualMealDiv.className = "individual-meal";
-    const mealInfo = `
-        <img src="${meal.strMealThumb}">
-        <h1>${meal.strMeal}</h1>`;
-    individualMealDiv.innerHTML = mealInfo;
+    individualMealDiv.innerHTML = `
+      <img src="${meal.strMealThumb}">
+      <h4>${meal.strMeal}</h4>
+    `;
     allMealsDiv.appendChild(individualMealDiv);
-    // console.log(meal);
+
     individualMealDiv.addEventListener("click", () => {
       individualMealIfo.innerHTML = `
-        <img src="${meal.strMealThumb}">
-        <h1>${meal.strMeal}</h1>
-        <ul>
-            <li class="list-item">${meal.strMeasure1} ${meal.strIngredient1}</li>
-            <li class="list-item">${meal.strMeasure2} ${meal.strIngredient2}</li>
-            <li class="list-item">${meal.strMeasure3} ${meal.strIngredient3}</li>
-            <li class="list-item">${meal.strMeasure4} ${meal.strIngredient4}</li>
-            <li class="list-item">${meal.strMeasure5} ${meal.strIngredient5}</li>
-            <li class="list-item">${meal.strMeasure6} ${meal.strIngredient6}</li>
-            <li class="list-item">${meal.strMeasure7} ${meal.strIngredient7}</li>
-            <li class="list-item">${meal.strMeasure8} ${meal.strIngredient8}</li>
-            <li class="list-item">${meal.strMeasure9} ${meal.strIngredient9}</li>
-            <li class="list-item">${meal.strMeasure10} ${meal.strIngredient10}</li>
-            <li class="list-item">${meal.strMeasure11} ${meal.strIngredient11}</li>
-            <li class="list-item">${meal.strMeasure12} ${meal.strIngredient12}</li>
-            <li class="list-item">${meal.strMeasure13} ${meal.strIngredient13}</li>
-            <li class="list-item">${meal.strMeasure14} ${meal.strIngredient14}</li>
-            <li class="list-item">${meal.strMeasure15} ${meal.strIngredient15}</li>
-            <li class="list-item">${meal.strMeasure16} ${meal.strIngredient16}</li>
-            <li class="list-item">${meal.strMeasure17} ${meal.strIngredient17}</li>
-            <li class="list-item">${meal.strMeasure18} ${meal.strIngredient18}</li>
-            <li class="list-item">${meal.strMeasure19} ${meal.strIngredient19}</li>
-            <li class="list-item">${meal.strMeasure20} ${meal.strIngredient20}</li>
-        </ul>`;
+          <img src="${meal.strMealThumb}">
+          <h1>${meal.strMeal}</h1>
+          <ul>
+              <li class="list-item">${meal.strMeasure1} ${meal.strIngredient1}</li>
+              <li class="list-item">${meal.strMeasure2} ${meal.strIngredient2}</li>
+              <li class="list-item">${meal.strMeasure3} ${meal.strIngredient3}</li>
+              <li class="list-item">${meal.strMeasure4} ${meal.strIngredient4}</li>
+              <li class="list-item">${meal.strMeasure5} ${meal.strIngredient5}</li>
+              <li class="list-item">${meal.strMeasure6} ${meal.strIngredient6}</li>
+              <li class="list-item">${meal.strMeasure7} ${meal.strIngredient7}</li>
+              <li class="list-item">${meal.strMeasure8} ${meal.strIngredient8}</li>
+              <li class="list-item">${meal.strMeasure9} ${meal.strIngredient9}</li>
+              <li class="list-item">${meal.strMeasure10} ${meal.strIngredient10}</li>
+              <li class="list-item">${meal.strMeasure11} ${meal.strIngredient11}</li>
+              <li class="list-item">${meal.strMeasure12} ${meal.strIngredient12}</li>
+              <li class="list-item">${meal.strMeasure13} ${meal.strIngredient13}</li>
+              <li class="list-item">${meal.strMeasure14} ${meal.strIngredient14}</li>
+              <li class="list-item">${meal.strMeasure15} ${meal.strIngredient15}</li>
+              <li class="list-item">${meal.strMeasure16} ${meal.strIngredient16}</li>
+              <li class="list-item">${meal.strMeasure17} ${meal.strIngredient17}</li>
+              <li class="list-item">${meal.strMeasure18} ${meal.strIngredient18}</li>
+              <li class="list-item">${meal.strMeasure19} ${meal.strIngredient19}</li>
+              <li class="list-item">${meal.strMeasure20} ${meal.strIngredient20}</li>
+          </ul>`;
 
       const ingredientsLists = document.getElementsByClassName("list-item");
 
@@ -94,6 +81,8 @@ const displayMeals = (mealName) => {
           ].innerHTML = `&#10149; ${ingredientsLists[i].innerText}`;
         }
       }
+
+      window.scrollTo(0, 0);
     });
   });
 };
